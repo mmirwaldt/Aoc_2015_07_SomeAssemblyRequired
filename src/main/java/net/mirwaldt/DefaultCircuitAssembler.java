87913e883fdb81de4f5af65524c7ef82d4b616f2
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-18 02:29:37 +0100

    expression interface with static classes extracted from DefaultCircuitAssembler

--- src/main/java/net/mirwaldt/DefaultCircuitAssembler.java
+++ src/main/java/net/mirwaldt/DefaultCircuitAssembler.java
@@ -10,67 +10,76 @@ public class DefaultCircuitAssembler implements CircuitAssembler {
     public void assemble(String circuit) {
         final String[] tokens = circuit.trim().split(" ");
         if(tokens.length == 3) {
-            try {
-                expressionByVars.put(tokens[2], new Value(Integer.parseInt(tokens[0])));
-            } catch (NumberFormatException e) {
-                expressionByVars.put(tokens[2], new Variable(expressionByVars, tokens[0]));
-            }
+            createValueOrVarAssignment(tokens);
         } else if(tokens.length == 4 && tokens[0].equals("NOT")) {
-            expressionByVars.put(tokens[3], new Not(new Variable(expressionByVars, tokens[1])));
+            expressionByVars.put(tokens[3], new Expression.Not(new Expression.Variable(expressionByVars, tokens[1])));
         } else if(tokens.length == 5) {
-            final String operator = tokens[1];
-            final String left = tokens[0];
-            final String right = tokens[2];
-            final String result = tokens[4];
-            final Integer leftInt = tryParseInt(left);
-            final Integer rightInt = tryParseInt(right);
-            if(operator.equals("AND")) {
-                createAnd(left, right, result, leftInt, rightInt);
-            } else if(operator.equals("OR")) {
-                createOr(left, right, result, leftInt, rightInt);
-            } else if(operator.equals("LSHIFT")) {
-                expressionByVars.put(result,
-                        new LShift(new Variable(expressionByVars, left), Integer.parseInt(right)));
-            } else if(operator.equals("RSHIFT")) {
-                expressionByVars.put(result,
-                        new RShift(new Variable(expressionByVars, left), Integer.parseInt(right)));
-            } else {
-                throw new IllegalArgumentException("Unknown operator '" + operator + "'");
-            }
+            createBinaryExpression(tokens);
         } else {
             throw new IllegalArgumentException("Wrong syntax in circuit '" + circuit + "'");
+        }
+    }
+
+    private void createValueOrVarAssignment(String[] tokens) {
+        final Integer anInt = tryParseInt(tokens[0]);
+        if(anInt == null) {
+            expressionByVars.put(tokens[2], new Expression.Variable(expressionByVars, tokens[0]));
+        } else {
+            expressionByVars.put(tokens[2], new Expression.Value(anInt));
+        }
+    }
+
+    private void createBinaryExpression(String[] tokens) {
+        final String operator = tokens[1];
+        final String left = tokens[0];
+        final String right = tokens[2];
+        final String result = tokens[4];
+        final Integer leftInt = tryParseInt(left);
+        final Integer rightInt = tryParseInt(right);
+        if(operator.equals("AND")) {
+            createAnd(left, right, result, leftInt, rightInt);
+        } else if(operator.equals("OR")) {
+            createOr(left, right, result, leftInt, rightInt);
+        } else if(operator.equals("LSHIFT")) {
+            expressionByVars.put(result,
+                    new Expression.LShift(new Expression.Variable(expressionByVars, left), rightInt));
+        } else if(operator.equals("RSHIFT")) {
+            expressionByVars.put(result,
+                    new Expression.RShift(new Expression.Variable(expressionByVars, left), rightInt));
+        } else {
+            throw new IllegalArgumentException("Unknown operator '" + operator + "'");
         }
     }
 
     private void createOr(String left, String right, String result, Integer leftInt, Integer rightInt) {
         if(leftInt == null && rightInt == null) {
             expressionByVars.put(result,
-                    new Or(new Variable(expressionByVars, left),
-                            new Variable(expressionByVars, right)));
+                    new Expression.Or(new Expression.Variable(expressionByVars, left),
+                            new Expression.Variable(expressionByVars, right)));
         } else if(leftInt == null) {
             expressionByVars.put(result,
-                    new Or(new Variable(expressionByVars, left),
-                            new Value(rightInt)));
+                    new Expression.Or(new Expression.Variable(expressionByVars, left),
+                            new Expression.Value(rightInt)));
         } else {
             expressionByVars.put(result,
-                    new Or(new Value(leftInt),
-                            new Variable(expressionByVars, right)));
+                    new Expression.Or(new Expression.Value(leftInt),
+                            new Expression.Variable(expressionByVars, right)));
         }
     }
 
     private void createAnd(String left, String right, String result, Integer leftInt, Integer rightInt) {
         if(leftInt == null && rightInt == null) {
             expressionByVars.put(result,
-                    new And(new Variable(expressionByVars, left),
-                            new Variable(expressionByVars, right)));
+                    new Expression.And(new Expression.Variable(expressionByVars, left),
+                            new Expression.Variable(expressionByVars, right)));
         } else if(leftInt == null) {
             expressionByVars.put(result,
-                    new And(new Variable(expressionByVars, left),
-                            new Value(rightInt)));
+                    new Expression.And(new Expression.Variable(expressionByVars, left),
+                            new Expression.Value(rightInt)));
         } else {
             expressionByVars.put(result,
-                    new And(new Value(leftInt),
-                            new Variable(expressionByVars, right)));
+                    new Expression.And(new Expression.Value(leftInt),
+                            new Expression.Variable(expressionByVars, right)));
         }
     }
 
@@ -91,116 +100,5 @@ public class DefaultCircuitAssembler implements CircuitAssembler {
             result.put(expressionByVarEntry.getKey(), expressionByVarEntry.getValue().eval());
         }
         return result;
-    }
-
-    interface Expression {
-        int eval();
-    }
-
-    static class Variable implements Expression {
-        private final SortedMap<String, Expression> expressionByVars;
-        private final String variableName;
-
-        public Variable(SortedMap<String, Expression> expressionByVars, String variableName) {
-            this.expressionByVars = expressionByVars;
-            this.variableName = variableName;
-        }
-
-        @Override
-        public int eval() {
-            final Expression expression = expressionByVars.get(variableName);
-            if(expression == null) {
-                throw new NoSuchElementException("No expression for variable '" + variableName + "'.");
-            }
-            final int value = expression.eval() & 0xFFFF;
-            expressionByVars.put(variableName, new Value(value));
-            return value;
-        }
-    }
-
-    static class Value implements Expression {
-        private final int value;
-
-        public Value(int value) {
-            this.value = value;
-        }
-
-        @Override
-        public int eval() {
-            return value & 0xFFFF;
-        }
-    }
-
-    static class Not implements Expression {
-        private final Expression expression;
-
-        public Not(Expression expression) {
-            this.expression = expression;
-        }
-
-        @Override
-        public int eval() {
-            return (~expression.eval()) & 0xFFFF;
-        }
-    }
-
-    static class LShift implements Expression {
-        private final Expression expression;
-        private final int numberOfBits;
-
-        public LShift(Expression expression, int numberOfBits) {
-            this.expression = expression;
-            this.numberOfBits = numberOfBits;
-        }
-
-        @Override
-        public int eval() {
-            return (expression.eval() << numberOfBits) & 0xFFFF;
-        }
-    }
-
-    static class RShift implements Expression {
-        private final Expression expression;
-        private final int numberOfBits;
-
-        public RShift(Expression expression, int numberOfBits) {
-            this.expression = expression;
-            this.numberOfBits = numberOfBits;
-        }
-
-        @Override
-        public int eval() {
-            return (expression.eval() >> numberOfBits) & 0xFFFF;
-        }
-    }
-
-    static class And implements Expression {
-        private final Expression left;
-        private final Expression right;
-
-        public And(Expression left, Expression right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public int eval() {
-            return (left.eval() & right.eval()) & 0xFFFF;
-        }
-    }
-
-    static class Or implements Expression {
-        private final Expression left;
-        private final Expression right;
-
-        public Or(Expression left, Expression right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public int eval() {
-            return (left.eval() | right.eval()) & 0xFFFF;
-        }
     }
 }
---
+++ src/main/java/net/mirwaldt/Expression.java
@@ -0,0 +1,115 @@
+package net.mirwaldt;
+
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+
+public interface Expression {
+    int eval();
+
+    class Variable implements Expression {
+        private final SortedMap<String, Expression> expressionByVars;
+        private final String variableName;
+
+        public Variable(SortedMap<String, Expression> expressionByVars, String variableName) {
+            this.expressionByVars = expressionByVars;
+            this.variableName = variableName;
+        }
+
+        @Override
+        public int eval() {
+            final Expression expression = expressionByVars.get(variableName);
+            if(expression == null) {
+                throw new NoSuchElementException("No expression for variable '" + variableName + "'.");
+            }
+            final int value = expression.eval() & 0xFFFF;
+            expressionByVars.put(variableName, new Value(value));
+            return value;
+        }
+    }
+
+    class Value implements Expression {
+        private final int value;
+
+        public Value(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public int eval() {
+            return value & 0xFFFF;
+        }
+    }
+
+    class Not implements Expression {
+        private final Expression expression;
+
+        public Not(Expression expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public int eval() {
+            return (~expression.eval()) & 0xFFFF;
+        }
+    }
+
+    class LShift implements Expression {
+        private final Expression expression;
+        private final int numberOfBits;
+
+        public LShift(Expression expression, int numberOfBits) {
+            this.expression = expression;
+            this.numberOfBits = numberOfBits;
+        }
+
+        @Override
+        public int eval() {
+            return (expression.eval() << numberOfBits) & 0xFFFF;
+        }
+    }
+
+    class RShift implements Expression {
+        private final Expression expression;
+        private final int numberOfBits;
+
+        public RShift(Expression expression, int numberOfBits) {
+            this.expression = expression;
+            this.numberOfBits = numberOfBits;
+        }
+
+        @Override
+        public int eval() {
+            return (expression.eval() >> numberOfBits) & 0xFFFF;
+        }
+    }
+
+    class And implements Expression {
+        private final Expression left;
+        private final Expression right;
+
+        public And(Expression left, Expression right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int eval() {
+            return (left.eval() & right.eval()) & 0xFFFF;
+        }
+    }
+
+    class Or implements Expression {
+        private final Expression left;
+        private final Expression right;
+
+        public Or(Expression left, Expression right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int eval() {
+            return (left.eval() | right.eval()) & 0xFFFF;
+        }
+    }
+}
